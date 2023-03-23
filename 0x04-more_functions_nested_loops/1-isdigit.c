bf8919d982e0b3cf0e7ade4eb43f16d75d39a2cb
Anas8e <bellacheanas@gmail.com>
2023-03-23 16:58:30 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _isupper - checkes for uppercase character
+  * @c: the character
+  *
+  * Return: 1 if there is uppercase or 0 otherwise
+  */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
