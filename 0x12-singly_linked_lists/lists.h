ffc7998c3076746f8e60f91cc5347b971450a630
Anas8e <bellacheanas@gmail.com>
2023-04-27 17:11:50 -0700

    0 second try

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
 
 /**
  * struct list_s - singly linked list
@@ -11,14 +14,15 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-/* Function prototypes */
+int _putchar(char c);
 size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
